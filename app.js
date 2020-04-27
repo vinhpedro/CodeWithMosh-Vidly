25ff2089bd12c71c6f1c1c18869f4fce860f08fb
Ash Robinson <a_p_robinson@icloud.com>
2020-04-27 20:52:16 +0100

    Moved homepage route to new file, home.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const Joi = require('@hapi/joi');
+const home=require('./routes/home');
 const genres=require('./routes/genres');
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/genres', genres);
-
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-});
+app.use('/', home);
 
 const port = process.env.PORT || 3000;
 
