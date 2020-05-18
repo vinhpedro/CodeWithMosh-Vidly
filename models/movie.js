ba1ca7316c1733bcb814a41791240923f487ad0b
Ash Robinson <a_p_robinson@icloud.com>
2020-05-18 20:26:40 +0100

    Added exports and genreSchema to Movie model

--- models/movie.js
+++ models/movie.js
@@ -1,19 +1,38 @@
 const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
+const {genreSchema} = require('./genre');
 
 const movieSchema = new mongoose.Schema({
-    title: {
+    title:{
         type:String,
         required:true
-    },
+    }
 
     genre:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref: 'Genre'
-    },
+        type:genreSchema,
+        required:true
+    }
 
+    numberInStock:{
+        type:Number,
+        required:true
+    }
+
+    dailyRentalRate:{
+        type:Number,
+        required:true
+    }
 })
 
 const Movie = mongoose.model('Movie', movieSchema);
 
+const validateMovie = (movie) => {
+    const schema = Joi.object({
+        title:Joi.string().min(3).required()
+    })
+
+    return schema.validate(movie);
+}
+
 exports.Movie = Movie;
+exports.validate = validateMovie;
