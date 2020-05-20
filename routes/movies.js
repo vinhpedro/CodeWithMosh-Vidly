a3664cdc66f2fe824b3df2e9a4d8b0d52ae75424
Ash Robinson <a_p_robinson@icloud.com>
2020-05-20 20:19:42 +0100

    Added PUT and DELETE methods to movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -48,4 +48,42 @@ router.post('/', async (req, res) => {
     res.send(movie);
 })
 
+router.put('/:id', async (req, res)=>{
+
+    const { error } = validate(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    
+    if(!genre) return res.status(400).send('Invalid genre given...')
+
+    let movie = await Movie.findByIdAndUpdate(req.params.id,{
+        $set:{
+            title:req.body.title,
+            genre:{
+                _id: genre._id,
+                name: genre.name
+            },
+            numberInStock:req.body.numberInStock,
+            dailyRentalRate:req.body.dailyRentalRate
+        }
+    }, {new:true})
+
+    if (!movie) return res.status(404).send("No movie found for that ID...");
+
+    movie = await movie.save();
+
+    res.send(movie);
+
+})
+
+router.delete('/:id', async (req, res)=>{
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+
+    if (!movie) res.status(404).send("No movie found for that ID...");
+
+    res.send(movie);
+})
+
 module.exports = router;
