ee3a39e54effab0086ec23c1112a50f7a87a4979
Ash Robinson <a_p_robinson@icloud.com>
2020-05-21 20:37:19 +0100

    Added initial code to POST method.

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router=express.Router();
-const {Rental,validate} = require('../models/rental')
+const {Rental,validate} = require('../models/rental');
+const {Customer} = require('../models/customer');
+const {Movie} = require('../models/movie');
 
 router.get('/', async (req, res) =>{
     const rentals = await Rental
         .find()
+        .sort('-dateOut')
 
     res.send(rentals);
 })
 
 router.post('/', async (req, res)=>{
-    let rental = await new Rental({
 
+    const {error} = validate(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = Customer.findById(req.body.customerId);
+
+    if(!customer) return res.status(400).send('Invalid customer ID...')
+
+    const movie = Movie.findById(req.body.movieId);
+
+    if(!movie) return res.status(400).send('Invalid movie ID...')
+    
+    let rental = await new Rental({
+        customer:{
+            _id:customer._id,
+            name:customer.name,
+            phone:customer.phone
+        },
+        movie:{
+            _id:movie._id,
+            title:movie.title,
+            dailyRentalRate:movie.dailyRentalRate
+        }
     })
 
     rental = await rental.save();
+
+    movie.numberInStock--;
+    movie.save();
 
     res.send(rental);
 })
