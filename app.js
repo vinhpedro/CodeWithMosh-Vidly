4b367a0bd4d843fd7ce43fa25794b08baa79c399
Ash Robinson <a_p_robinson@icloud.com>
2020-05-22 19:19:39 +0100

    Set useFindAndModify to false within mongoose.connect, and created PUT method in rentals.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const rentals=require('./routes/rentals');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false})
     .then(() => {console.log('Connected to MongoDB')})
     .catch(err => console.error(err))
 
--- routes/rentals.js
+++ routes/rentals.js
@@ -63,6 +63,41 @@ router.post('/', async (req, res)=>{
     }
 })
 
+router.put('/:id', async (req, res) => {
+
+    const {error} = validate(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findById(req.body.customerId);
+
+    if(!customer) return res.status(400).send('Invalid customer ID...')
+
+    const movie = await Movie.findById(req.body.movieId);
+
+    if(!movie) return res.status(400).send('Invalid movie ID...')
+
+    const rental = await Rental.findByIdAndUpdate(req.params.id,{
+        $set:{
+            customer:{
+                _id:customer._id,
+                name:customer.name,
+                phone:customer.phone
+            },
+            movie:{
+                _id:movie._id,
+                title:movie.title,
+                dailyRentalRate:movie.dailyRentalRate
+            }
+        }
+    },{new:true})
+
+    if (!rental) res.status(404).send("No rental found for that ID...");
+
+    res.send(rental);
+
+})
+
 router.get('/:id', async (req, res) => {
 
     const rental = await Rental
