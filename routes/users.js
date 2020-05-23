faf348d6bdedb013ff997be67ab04ebe679a93cc
Ash Robinson <a_p_robinson@icloud.com>
2020-05-23 16:15:25 +0100

    Added hashing to user passwrods with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const router=express.Router();
 const { User, validate } = require('../models/user');
 
@@ -13,17 +15,20 @@ router.post('/', async (req, res) => {
 
     const {error} = validate(req.body);
 
-    if(error) return res.status(404).send(error.details[0].message);
+    if(error) return res.status(400).send(error.details[0].message);
 
-    const user = new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    })
+    let user = await User.findOne({ email:req.body.email })
+
+    if(user) return res.status(400).send('User already registered...');
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
     await user.save();
 
-    res.send(user);
+    res.send(_.pick(user, ['name', 'email']));
 
 })
 
