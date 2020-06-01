54037d3f3d027a1c7729ddaa6014ebf0050d1c85
Ash Robinson <a_p_robinson@icloud.com>
2020-06-01 20:14:00 +0100

    Moved error logging logic from app.js to dedicated module in /startup directory

--- app.js
+++ app.js
@@ -2,33 +2,12 @@ const express = require('express');
 const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const config = require('config');
-const winston = require('winston');
-require('winston-mongodb');
-require('express-async-errors');
 
 const app = express();
 
+require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
-
-winston.exceptions.handle(
-    new winston.transports.File({filename: 'uncaughtExceptions.log'})
-);
-
-process.on('uncaughtException', (ex) => {
-    winston.error(ex.message);
-    process.exit(1);
-});
-
-process.on('unhandledRejection', (ex) => {
-    winston.error(ex.message);
-    process.exit(1);
-});
-
-winston.add(new winston.transports.File({filename: 'logfile.log'}));
-winston.add(new (winston.transports.MongoDB)( {
-    db: 'mongodb://localhost/vidly'
-} ));
 
 if(!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined...')
---
+++ startup/logging.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+
+module.exports=function(){
+
+    winston.exceptions.handle(
+        new winston.transports.File({filename: 'uncaughtExceptions.log'})
+    );
+
+    process.on('uncaughtException', (ex) => {
+        winston.error(ex.message);
+        process.exit(1);
+    });
+
+    process.on('unhandledRejection', (ex) => {
+        winston.error(ex.message);
+        process.exit(1);
+    });
+
+    winston.add(new winston.transports.File({filename: 'logfile.log'}));
+    winston.add(new (winston.transports.MongoDB)( {
+        db: 'mongodb://localhost/vidly'
+    } ));
+};
