5223104610e0a63ab08ebb16394ccc1e0c7d563f
Ash Robinson <a_p_robinson@icloud.com>
2020-06-01 20:22:03 +0100

    Moved validation logic from app.js to dedicated module in /startup directory

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const Joi = require('@hapi/joi');
-Joi.objectId = require('joi-objectid')(Joi);
-
 const app = express();
 
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config');
+require('./startup/validation');
 
 const port = process.env.PORT || 3000;
 
