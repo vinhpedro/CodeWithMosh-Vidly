e09cdcdd18fce32f596d4a54f42fbc91e49b0318
Ash Robinson <a_p_robinson@icloud.com>
2020-06-01 19:47:12 +0100

    Moved routes from app.js to dedicated module in /startup directory

--- app.js
+++ app.js
@@ -7,16 +7,10 @@ const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
 
-const home=require('./routes/home');
-const genres=require('./routes/genres');
-const customers=require('./routes/customers');
-const movies=require('./routes/movies');
-const rentals=require('./routes/rentals');
-const users=require('./routes/users');
-const auth=require('./routes/auth');
-const error=require('./middleware/error');
-
 const app = express();
+
+require('./startup/routes')(app);
+require('./startup/database');
 
 process.on('uncaughtException', (ex) => {
     winston.error(ex.message);
@@ -37,20 +31,6 @@ if(!config.get('jwtPrivateKey')) {
     console.error('FATAL ERROR: jwtPrivateKey is not defined...')
     process.exit(1);
 }
-
-mongoose.connect('mongodb://localhost/vidly', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false})
-    .then(() => {console.log('Connected to MongoDB')})
-    .catch(err => console.error(err))
-
-app.use(express.json());
-app.use('/', home);
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 
